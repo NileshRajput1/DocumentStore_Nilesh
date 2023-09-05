7adf88cd837abe92a3b1199342d6111d466edd85
NileshRajput1 <singhraja501@gmail.com>
2023-09-05 15:05:57 +0530

    Commvault

---
+++ aws.py
@@ -0,0 +1,25 @@
+import boto3
+
+ec2 = boto3.client('ec2', region_name='us-east-1')
+
+response = ec2.run_instances(
+    ImageId='ami-051f7e7f6c2f40dc1',  
+    InstanceType='t2.micro',
+    MinCount=1,
+    MaxCount=1,
+    KeyName='nileshkey',  
+    SecurityGroupIds=['sg-0419d45415ceb7f71'], 
+)
+
+instance_id = response['Instances'][0]['InstanceId']
+waiter = ec2.get_waiter('instance_running')
+waiter.wait(InstanceIds=[instance_id])
+
+public_ip = '54.146.114.142'  
+ec2.associate_address(InstanceId=instance_id, PublicIp=public_ip)
+
+s3 = boto3.client('s3', region_name='us-east-1')  
+
+bucket_name = 'nilesh9245'
+
+s3.create_bucket(Bucket=bucket_name)
