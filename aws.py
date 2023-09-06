708c42a6a1461be8c717787ed6faecebed32a3a6
Nilesh  Kumar Singh <singhraja501@gmail.com>
2023-09-06 21:11:17 +0530

    changed sgID, keypair, elastic IP

--- aws.py
+++ aws.py
@@ -7,15 +7,15 @@ response = ec2.run_instances(
     InstanceType='t2.micro',
     MinCount=1,
     MaxCount=1,
-    KeyName='nileshkey',  
-    SecurityGroupIds=['sg-0419d45415ceb7f71'], 
+    KeyName='commvault_project',  
+    SecurityGroupIds=['sg-007aba5698f6d98c2'], 
 )
 
 instance_id = response['Instances'][0]['InstanceId']
 waiter = ec2.get_waiter('instance_running')
 waiter.wait(InstanceIds=[instance_id])
 
-public_ip = '54.146.114.142'  
+public_ip = '54.163.162.174'  
 ec2.associate_address(InstanceId=instance_id, PublicIp=public_ip)
 
 s3 = boto3.client('s3', region_name='us-east-1')  
